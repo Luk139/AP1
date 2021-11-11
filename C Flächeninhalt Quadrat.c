dfc82be08ea3c631caec3d9f61fc1e8ad7728c6d
Luk139 <79744371+Luk139@users.noreply.github.com>
2021-11-11 09:25:53 +0100

    Update C Flächeninhalt Quadrat.c

--- C Flächeninhalt Quadrat.c
+++ C Flächeninhalt Quadrat.c
@@ -2,9 +2,9 @@
 
 int main(){
 	int a, f;
-	printf("Geben sie die Seitenlänge des Quadrats an: ");
+	printf("Geben sie die Seitenlänge des Quadrats in cm an: ");
 	scanf("%d",&a);
 	f = a*a;
-	printf("Der Flächeninhalt beträgt %d ",f);
+	printf("Der Flächeninhalt beträgt %d cm",f);
 	return 0;
 }
