be9cf011d8da11f04c72aef2803e98ab92f5f66e
Luk139 <79744371+Luk139@users.noreply.github.com>
2021-11-11 09:25:21 +0100

    Add files via upload

---
+++ C Flächeninhalt Quadrat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	int a, f;
+	printf("Geben sie die Seitenlänge des Quadrats an: ");
+	scanf("%d",&a);
+	f = a*a;
+	printf("Der Flächeninhalt beträgt %d ",f);
+	return 0;
+}
