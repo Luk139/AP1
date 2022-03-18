f3245b901b685a71b308e725c277187e4e405788
Luk139 <79744371+Luk139@users.noreply.github.com>
2022-03-18 13:27:08 +0100

    Update Praktikum64.java

--- Praktikum/Praktikum64.java
+++ Praktikum/Praktikum64.java
@@ -1,21 +1,18 @@
-class Ackermann
-{
-  public static long ackermann( long n, long m )
-  {
-    if ( n == 0 )
-      return m + 1;
-    else
-    
-      if ( m == 0 )
-        return ackermann( n - 1, 1 );
-      else
-        return ackermann( n - 1, ackermann(n, m - 1) );
-    
-  }
-  public static void main( String args[] )
-  {
-    int x = 2,
-        y = 2;
-    System.out.println( "ackermann(" + x + "," + y + ")=" + ackermann(x,y) );
-  }
+public class Main{
+    public static void main(String[] args) {
+        //kann man auch mit einem scanner einlesen, wenn man nöchte, funktionalität ändert sich nicht
+        long M = 2;
+        long N = 2;
+        System.out.println(Ackermann.ackermann(M, N));
+    }
+}
+class Ackermann {
+
+    public static long ackermann(long m, long n) {
+        if (m == 0) return n + 1;
+        if (n == 0) return ackermann(m - 1, 1);
+        return ackermann(m - 1, ackermann(m, n - 1));
+    }
+
+
 }
